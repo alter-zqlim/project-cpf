3d6e56391c6d8569622d2534778bc0fcfa865b19
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-06 20:57:45 +0800

    Create 6_About_Us.py

---
+++ pages/6_About_Us.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title = "About Us"
+)
+
+st.title("About Us")
+
+st.subheader("This is a detailed page that outlines the project scope, objectives, data sources, and features.")
