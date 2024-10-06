f5aed765d31d5bc622f5ba53ea0e722adcc62ce9
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-06 20:54:37 +0800

    Create 5_Methodology.py

---
+++ pages/5_Methodology.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title = "Methodology"
+)
+
+st.title("Methodology")
+
+st.subheader("A comprehensive explanation of the data flows and implementation details.")
+st.subheader("A flowchart illustrating the process flow for each of the use cases in the application.")
+st.subheader("For example, if the application has two main use cases: a) chat with information and b) intelligent search, each of these use cases should have its own flowchart.")
+st.subheader("Refer to the sample here")
+st.subheader("Links to an external site. for the samples of the flowcharts and methodology (Slide 13, 14, and 15).")
