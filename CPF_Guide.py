59e7c2cc98d5ee3716fa93f7be4dcdae9b28acb9
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-07 07:18:14 +0800

    Update CPF_Guide.py

--- CPF_Guide.py
+++ CPF_Guide.py
@@ -3,3 +3,14 @@ import pandas as pd
 import numpy as np
 import langchain
 from helper_functions.utility import check_password  
+
+
+# for query
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+from langchain.retrievers.multi_query import MultiQueryRetriever
+from langchain_openai import ChatOpenAI
+
