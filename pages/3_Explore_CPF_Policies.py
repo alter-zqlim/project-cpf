be9934a3b47c02d8cda9377359f92a39ab10170b
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-08 09:50:28 +0800

    Update 3_Explore_CPF_Policies.py

--- pages/3_Explore_CPF_Policies.py
+++ pages/3_Explore_CPF_Policies.py
@@ -24,5 +24,5 @@ user_prompt = form.text_area(
 )
 
 if form.form_submit_button("Submit"):
-    st.toast("User Input Submitted - {user_prompt}")
+    st.toast(f"User Input Submitted - {user_prompt}")
     print(f"User Input is {user_prompt}")
