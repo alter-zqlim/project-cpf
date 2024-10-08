ba89f98a64e35b4a0fe81db5dacaf86d6256b2a3
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-08 14:17:18 +0800

    Update 1_llm.py

--- pages/1_llm.py
+++ pages/1_llm.py
@@ -9,3 +9,5 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.document_loaders import PyPDFLoader
 loader = PyPDFLoader("https://www.developer.tech.gov.sg/products/collections/data-science-and-artificial-intelligence/playbooks/prompt-engineering-playbook-beta-v3.pdf")
 pages = loader.load()
+
+st.write(pages[0])
