c2da5734825fd1770116c2d8c05d14c16f406747
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-08 12:19:42 +0800

    Update 1_llm.py

--- pages/1_llm.py
+++ pages/1_llm.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from helper_functions.utility import check_password
 from helper_functions import llm
+import langchain_community
 from langchain_community.document_loaders import PyPDFLoader
 
 loader = PyPDFLoader("https://www.developer.tech.gov.sg/products/collections/data-science-and-artificial-intelligence/playbooks/prompt-engineering-playbook-beta-v3.pdf")
