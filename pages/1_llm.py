f19304c9bb2482cdee4f387c6de0d759b74c82df
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-08 14:03:21 +0800

    Update 1_llm.py

--- pages/1_llm.py
+++ pages/1_llm.py
@@ -5,3 +5,7 @@ from helper_functions import llm
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+from langchain.document_loaders import PyPDFLoader
+loader = PyPDFLoader("https://www.developer.tech.gov.sg/products/collections/data-science-and-artificial-intelligence/playbooks/prompt-engineering-playbook-beta-v3.pdf")
+pages = loader.load()
