220e5381146f3f3f6f526843f8adf977cec0a68d
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-08 09:46:41 +0800

    Update 3_Explore_CPF_Policies.py

--- pages/3_Explore_CPF_Policies.py
+++ pages/3_Explore_CPF_Policies.py
@@ -14,3 +14,15 @@ st.write(
 # password checkpoint
 if not check_password():  
     st.stop()
+
+form = st.form(key = "form")
+form.subheader("Prompt")
+
+user_prompt = form.text_area(
+    "Enter your prompt here", 
+    height = 160
+)
+
+if form.form_submit_button("Submit"):
+    st.toast("User Input Submitted - {user_prompt}")
+    print(f"User Input is {user_prompt}")
