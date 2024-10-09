c596be54e2d62158cd7cdccd92da9e581c518ba9
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-09 12:38:07 +0800

    Update 6_About_Us.py

--- pages/6_About_Us.py
+++ pages/6_About_Us.py
@@ -7,3 +7,6 @@ st.set_page_config(
 st.title("About Us")
 
 st.subheader("This is a detailed page that outlines the project scope, objectives, data sources, and features.")
+
+st.subheader("Data sources")
+st.write("Educational resources on https://www.cpf.gov.sg/member/infohub/educational-resources relating to the topic of 'Investment'.")
