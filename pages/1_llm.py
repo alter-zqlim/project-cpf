a09f9c18a87e6d7f03c5c8ca9d3a64c19d6c8b1a
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-09 11:55:18 +0800

    Update 1_llm.py

--- pages/1_llm.py
+++ pages/1_llm.py
@@ -105,14 +105,7 @@ splitted_documents = text_splitter.split_documents(pages)
 
 st.write(len(splitted_documents))
 st.write(splitted_documents[17])
-"""
-db = Chroma.from_documents(
-    splitted_documents,
-    embeddings_model,
-    persist_directory="./chroma_db"
-)
-st.write(db._collection.count())
-"""
+
 
 loader_web = WebBaseLoader("https://lilianweng.github.io/posts/2023-06-23-agent/")
 data = loader_web.load()
