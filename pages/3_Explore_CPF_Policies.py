8a373c07b541e20553e901ea2a423241b9475d07
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-09 14:03:16 +0800

    Update 3_Explore_CPF_Policies.py

--- pages/3_Explore_CPF_Policies.py
+++ pages/3_Explore_CPF_Policies.py
@@ -7,6 +7,9 @@ from langchain_openai import ChatOpenAI
 from langchain_core.vectorstores import InMemoryVectorStore
 from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+from langchain_core.prompts import ChatPromptTemplate
+
 llmodel = ChatOpenAI(model = "gpt-4o-mini")
 
 st.set_page_config(
@@ -23,6 +26,35 @@ st.write(
 if not check_password():  
     st.stop()
 
+# prep source file
+file_path = "./data/food-waste-minimisation-guidebook-for-food-manufacturing-establishments.pdf"
+loader = PyPDFLoader(file_path)
+docs = loader.load()
+st.write(len(docs))
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 1000,
+    chunk_overlap = 200
+)
+
+splits = text_splitter.split_documents(docs)
+vectorstore = InMemoryVectorStore.from_documents(
+    documents = splits,
+    embedding = OpenAIEmbeddings(model = 'text-embedding-3-small')
+)
+
+retriever = vectorstore.as_retriever()
+
+system_prompt = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't know. Use three sentences maximum and keep the "
+    "answer concise."
+    "\n\n"
+    "{context}"
+)
+
 form = st.form(key = "form")
 form.subheader("What would you like to know about CPF policies?")
 
@@ -36,19 +68,3 @@ if form.form_submit_button("Submit"):
     response = llm.get_completion(user_prompt)
     st.write(response)
     print(f"User Input is {user_prompt}")
-
-
-file_path = "./data/food-waste-minimisation-guidebook-for-food-manufacturing-establishments.pdf"
-loader = PyPDFLoader(file_path)
-docs = loader.load()
-st.write(len(docs))
-st.write(docs[0].page_content[0:100])
-st.write(docs[0].metadata)
-
-text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 200)
-splits = text_splitter.split_documents(docs)
-vectorstore = InMemoryVectorStore.from_documents(
-    documents = splits, embedding = OpenAIEmbeddings(model='text-embedding-3-small')
-)
-
-retriever = vectorstore.as_retriever()
