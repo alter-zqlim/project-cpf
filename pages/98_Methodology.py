93f10af368957f557af5a56be011f9c481977f2d
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-10 23:22:49 +0800

    Update 98_Methodology.py

--- pages/98_Methodology.py
+++ pages/98_Methodology.py
@@ -12,8 +12,9 @@ st.subheader("For example, if the application has two main use cases: a) chat wi
 st.subheader("Refer to the sample here")
 st.subheader("Links to an external site. for the samples of the flowcharts and methodology (Slide 13, 14, and 15).")
 
+st.write("Watercolour ink image of a flooded post-apocalyptic street lined with abandoned shophouses of varying heights and sizes. The sky is cloudy and rust red.")
+
 st.image(
     "street.jpg",
     caption = "Watercolour ink image of a flooded post-apocalyptic street lined with abandoned shophouses of varying heights and sizes. The sky is cloudy and rust red."
 )
-st.write("Watercolour ink image of a flooded post-apocalyptic street lined with abandoned shophouses of varying heights and sizes. The sky is cloudy and rust red.")
