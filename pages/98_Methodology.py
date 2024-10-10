dcdb0652fcc8dd400ac536648c20487ac499c69a
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-10 23:22:22 +0800

    Update 98_Methodology.py

--- pages/98_Methodology.py
+++ pages/98_Methodology.py
@@ -16,3 +16,4 @@ st.image(
     "street.jpg",
     caption = "Watercolour ink image of a flooded post-apocalyptic street lined with abandoned shophouses of varying heights and sizes. The sky is cloudy and rust red."
 )
+st.write("Watercolour ink image of a flooded post-apocalyptic street lined with abandoned shophouses of varying heights and sizes. The sky is cloudy and rust red.")
