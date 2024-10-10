919da22ebfcc777a1f2373ce38b01692ebf3ece8
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-10 23:25:43 +0800

    Update 98_Methodology.py

--- pages/98_Methodology.py
+++ pages/98_Methodology.py
@@ -5,9 +5,15 @@ st.set_page_config(
 )
 
 st.title("Methodology")
-st.write("Watercolour ink image of a flooded post-apocalyptic street lined with abandoned shophouses of varying heights and sizes. The sky is cloudy and rust red.")
+
+st.subheader("A comprehensive explanation of the data flows and implementation details.")
+st.subheader("A flowchart illustrating the process flow for each of the use cases in the application.")
+st.subheader("For example, if the application has two main use cases: a) chat with information and b) intelligent search, each of these use cases should have its own flowchart.")
+st.subheader("Refer to the sample here")
+st.subheader("Links to an external site. for the samples of the flowcharts and methodology (Slide 13, 14, and 15).")
 
 st.image(
-    "street.jpg",
+    "../assets/street.jpg",
     caption = "Watercolour ink image of a flooded post-apocalyptic street lined with abandoned shophouses of varying heights and sizes. The sky is cloudy and rust red."
 )
+st.write("Watercolour ink image of a flooded post-apocalyptic street lined with abandoned shophouses of varying heights and sizes. The sky is cloudy and rust red.")
