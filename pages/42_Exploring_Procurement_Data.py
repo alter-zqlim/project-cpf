4c9f128e709ca00b0ca5ddd2ff992270164523fe
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-11 18:15:06 +0800

    Update and rename 42_Procurement_for_You.py to 42_Exploring_Procurement_Data.py

---
+++ pages/42_Exploring_Procurement_Data.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+from helper_functions.utility import check_password
+
+st.set_page_config(
+    page_title = "Exploring Procurement Data",
+    page_icon=":material/shopping_cart:"
+)
+
+st.title("Exploring Procurement Data")
+st.write(
+    "Users can explore different aspects of procurement data from GeBiz from FY2019 to FY2023."
+)
+
+# password checkpoint
+if not check_password():  
+    st.stop()
