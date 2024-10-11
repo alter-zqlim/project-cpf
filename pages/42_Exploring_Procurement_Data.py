a808d33cb6481f1d377ee4a032c2fc104fdd1b1e
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-11 18:56:28 +0800

    Update 42_Exploring_Procurement_Data.py

--- pages/42_Exploring_Procurement_Data.py
+++ pages/42_Exploring_Procurement_Data.py
@@ -22,10 +22,11 @@ def get_GeBIZ_data():
     return df.set_index("agency")
 
 df = get_GeBIZ_data()
-df_index = df.index.duplicated(keep='first')
+df_index = df[~df.index.duplicated(keep='first')]
+
 agencies = st.multiselect(
     "Select agencies",
-    list(df_index),
+    list(df_index.index),
     ["Competition and Consumer Commission of Singapore (CCCS)"]
 )
 if not agencies:
