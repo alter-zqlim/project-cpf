849943cb4cb5c3c8efb2f55f11116bbd950c97f6
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-11 18:28:36 +0800

    Update 42_Exploring_Procurement_Data.py

--- pages/42_Exploring_Procurement_Data.py
+++ pages/42_Exploring_Procurement_Data.py
@@ -20,3 +20,16 @@ if not check_password():
 def get_GeBIZ_data():
     df = pd.read_csv(".assets/GovernmentProcurementviaGeBIZ.csv")
     return df.set_index("agency")
+
+df = get_GeBIZ_data()
+agencies = st.multiselect(
+    "Select agencies",
+    list(df.index),
+    ["Competition and Consumer Commission of Singapore (CCCS)"]
+)
+if not agencies:
+    st.error("Please select at least one agency.")
+else:
+    data = df.loc[agencies]
+    data /= 1000000.0
+    st.write("### Gross Agricultural Production ($B)", data.sort_index())
