dab4faae9b86d4b39aa0853a929b00e13f72d3bf
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-12 11:59:58 +0800

    Update 99_About_Us.py

--- pages/99_About_Us.py
+++ pages/99_About_Us.py
@@ -4,11 +4,14 @@ st.set_page_config(
     page_title = "About Us"
 )
 
+# This is a detailed page that outlines the project scope, objectives, data sources, and features.
 st.title("About Us")
 
-st.subheader("This is a detailed page that outlines the project scope, objectives, data sources, and features.")
+st.subheader("Project scope", divider = True)
+st.subheader("Objectives", divider = True)
+st.subheader("Features", divider = True)
 
-st.subheader("\Large Data sources")
+st.subheader("Data sources", divider = True)
 st.write("Government Procurement via GeBIZ - Dataset of all open tenders put out by government agencies since FY2019. (retrieved via data.gov.sg on 11 October 2024)")
 st.write("Government Procurement - https://www.mof.gov.sg/policies/government-procurement (linked on 12 October 2024)")
 st.write("Guide to Singapore Procurement - https://www.gebiz.gov.sg/singapore-government-procurement-regime.html (linked on 12 October 2024) and related documents:")
