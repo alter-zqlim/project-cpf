4192fcdb181e9bbaf88deaed7400ebc1c67a5a92
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-12 12:35:15 +0800

    Update and rename 42_Exploring_Procurement_Data.py to 52_Exploring_Procurement_Data.py

--- pages/52_Exploring_Procurement_Data.py
+++ pages/52_Exploring_Procurement_Data.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from helper_functions.utility import check_password
+from helper_functions import llm
 
 st.set_page_config(
     page_title = "Exploring Procurement Data",
@@ -9,7 +10,7 @@ st.set_page_config(
 
 st.title("Exploring Procurement Data")
 st.write(
-    "Users can explore different aspects of procurement data from GeBiz from FY2019 to FY2023."
+    "Users can explore procurement data from GeBiz, based on various procuring agencies, from FY2019 to FY2023."
 )
 
 # password checkpoint
@@ -36,4 +37,26 @@ else:
     data = df.loc[agencies]
     st.write("## Procurement projects", data.sort_index())
 
-    # data = data.T.reset_index()
+
+def process_user_message(user_input):
+    delimiter = "```"
+    answer = ""
+    # check_for_malicious_intent
+    if llm.check_for_malicious_intent(user_input) == 'Y':
+        answer = "We are unable to process your request. Please rephrase your query or try a different query."
+    else:
+        answer = "Thank you for your query!"
+    return answer
+
+form = st.form(key = "form")
+form.subheader("What would you like to know about the above GeBiz procurement data from FY2019 to FY2023?")
+
+user_prompt = form.text_area(
+    "Please enter your query below", 
+    height = 160
+)
+
+if form.form_submit_button("Submit"):
+    st.toast(f"User Input Submitted - {user_prompt}")
+    response = process_user_message(user_prompt)
+    st.write("answer")
