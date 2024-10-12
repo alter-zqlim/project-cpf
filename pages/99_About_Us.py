af0d29c43ca3cb9328eee4d8ea07890b8201f47f
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-12 09:40:26 +0800

    Update 99_About_Us.py

--- pages/99_About_Us.py
+++ pages/99_About_Us.py
@@ -9,4 +9,4 @@ st.title("About Us")
 st.subheader("This is a detailed page that outlines the project scope, objectives, data sources, and features.")
 
 st.subheader("Data sources")
-st.write("Educational resources on https://www.cpf.gov.sg/member/infohub/educational-resources relating to the topic of 'Investment'.")
+st.write("Government Procurement via GeBIZ - Dataset of all open tenders put out by government agencies since FY2019. (retrieved on 11 October 2024)")
