65e0d75e4a22c222634201a01993cc587573a9f6
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-13 08:23:10 +0800

    Update CPF_Guide.py

--- CPF_Guide.py
+++ CPF_Guide.py
@@ -34,8 +34,8 @@ expander.write(EXPANDER_NOTICE)
 
 # main page header
 st.write("# For businesses: How to go about providing goods and services to the government")
-st.write("Hello Entrepreneur! Congratulations on setting up your business and we hope you see ever increasing sales! We hope that through this web application, you can learn more about transacting with the government to deliver goods and services!")
-st.write("Please be advised that there are *NO* procurement opportunities on this web application. Head over to https://www.gebiz.gov.sg/ to search for those opportunities.")
+st.write("Hello Entrepreneur! Congratulations on setting up your business and we hope you see ever increasing sales! Through this web application, you can learn more about transacting with the government to deliver goods and services!")
+st.write("Note that there are **NO** procurement opportunities on this web application. Head over to https://www.gebiz.gov.sg/ to search for those opportunities.")
 
 # manifest sidebar
 # st.sidebar.success("Select a demo above.")
