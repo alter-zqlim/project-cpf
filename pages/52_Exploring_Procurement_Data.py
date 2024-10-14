cd7c6f3d104814222e79c27f79e30264ac40a2ca
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-14 13:56:07 +0800

    Update 52_Exploring_Procurement_Data.py

--- pages/52_Exploring_Procurement_Data.py
+++ pages/52_Exploring_Procurement_Data.py
@@ -38,7 +38,7 @@ df_markeddown = df.to_markdown()
 
 pandas_agent = create_pandas_dataframe_agent(
     ChatOpenAI(temperature = 0, model = "gpt-4o-mini"),
-    pd.read_csv("./data/GovernmentProcurementviaGeBIZ.csv"),
+    unsorted_df,
     verbose = True,
     agent_type = AgentType.OPENAI_FUNCTIONS,
     allow_dangerous_code = True
