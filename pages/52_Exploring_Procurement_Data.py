bf8b1b215faab1052c203f577016bd07e271ca48
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-14 13:15:31 +0800

    Update 52_Exploring_Procurement_Data.py

--- pages/52_Exploring_Procurement_Data.py
+++ pages/52_Exploring_Procurement_Data.py
@@ -28,7 +28,7 @@ if not utility.check_password():
 @st.cache_data
 def get_GeBIZ_data():
     unsorted_df = pd.read_csv("./data/GovernmentProcurementviaGeBIZ.csv")
-    return unsorted_df.set_index("agency")
+    return unsorted_df.set_index("agency", drop = False)
 
 # read GeBiz data file, set index to "agency", sort by specified columns, construct index with unique values
 unsorted_df = get_GeBIZ_data()
