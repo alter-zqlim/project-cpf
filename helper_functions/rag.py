d82e986fdfb7b299cf03269607f5f1fece62520f
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-15 21:41:52 +0800

    Update rag.py

--- helper_functions/rag.py
+++ helper_functions/rag.py
@@ -47,5 +47,9 @@ def write_vector_store(splitted_documents):
 def get_procurement_answer(user_query, vector_base):
     template = "Use the following pieces of context to answer the question at the end. If you don't know the answer, do not try to make up an answer. Just say that you don't know. Use three sentences maximum. Keep the answer as concise as possible. {context} Question: {question} Helpful Answer:"
     retrieval_qa_chat_prompt = PromptTemplate.from_template(template)
+    large_lang_model = ChatOpenAI(model = "gpt-3.5-turbo", openai_api_key = st.secrets["KEY_OPENAI_API"])
     
+    combine_docs_chain = create_stuff_documents_chain(large_lang_model, retrieval_qa_chat_prompt)
+    rag_chain = create_retrieval_chain(vector_base, combine_docs_chain)
+    rag_chain.invoke({"input": user_query})
     return user_query
