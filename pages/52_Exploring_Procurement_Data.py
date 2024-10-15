d54be627b3ac585aaca458917880f80c67053f85
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-15 10:44:28 +0800

    Update 52_Exploring_Procurement_Data.py

--- pages/52_Exploring_Procurement_Data.py
+++ pages/52_Exploring_Procurement_Data.py
@@ -42,21 +42,8 @@ df_markeddown = df.to_markdown()
 KEY_OPENAI = st.secrets["KEY_OPENAI_API"]
 client = OpenAI(api_key = KEY_OPENAI)
 
-pandas_agent = create_pandas_dataframe_agent(
-    ChatOpenAI(temperature = 0, model = "gpt-4o-mini"),
-    unsorted_df,
-    verbose = True,
-    agent_type = AgentType.OPENAI_FUNCTIONS,
-    allow_dangerous_code = True
-)
-
-csv_agent = create_csv_agent(
-    ChatOpenAI(temperature = 0, model = "gpt-4o-mini"), 
-    "./data/GovernmentProcurementviaGeBIZ.csv",
-    verbose = True,
-    agent_type = AgentType.OPENAI_FUNCTIONS,
-    allow_dangerous_code = True
-)
+pandas_agent = llm.init_pandas_dataframe_agent(unsorted_df)
+csv_agent = llm.init_csv_agent("./data/GovernmentProcurementviaGeBIZ.csv")
 
 # generate a multi-option selector that displays data based on selected agencies  
 agencies = st.multiselect(
