74c8cb80d0bf17628b9f5f5e1dc37685c6cb47e2
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-15 17:58:09 +0800

    Update rag.py

--- helper_functions/rag.py
+++ helper_functions/rag.py
@@ -15,6 +15,7 @@ from langchain_openai import OpenAIEmbeddings
 
 __import__('pysqlite3')
 import sys
+import pysqlite3
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 def loader(filepath):
