b3b6d3c32c1f0c952ae7275eed6e1288ae26d537
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-15 21:29:28 +0800

    Update rag.py

--- helper_functions/rag.py
+++ helper_functions/rag.py
@@ -48,5 +48,4 @@ def get_procurement_answer(user_query, vector_base):
     template = "Use the following pieces of context to answer the question at the end. If you don't know the answer, do not try to make up an answer. Just say that you don't know. Use three sentences maximum. Keep the answer as concise as possible. {context} Question: {question} Helpful Answer:"
     retrieval_qa_chat_prompt = PromptTemplate.from_template(template)
     
-
     return user_query
