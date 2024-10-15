71d86ca455102272ee03347a0dc5a1a2d70a480f
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-15 21:18:00 +0800

    Update 53_Guide_to_Government_Procurement.py

--- pages/53_Guide_to_Government_Procurement.py
+++ pages/53_Guide_to_Government_Procurement.py
@@ -20,8 +20,12 @@ st.write(
 if not utility.check_password():  
     st.stop()
 
+# specify source
 debarment_guide = "./data/AUTHORITY_AND_RATIONALE_FOR_DEBARMENT.pdf"
-data_reference = rag.loader(debarment_guide)  # outputs 'pages'
-splitted_documents = rag.text_splitter(data_reference)
-db = rag.write_vector_store(splitted_documents)
+
+data_reference = rag.loader(debarment_guide)  # loads PDF
+splitted_documents = rag.text_splitter(data_reference)  # chunks loaded doc (PDF)
+db = rag.write_vector_store(splitted_documents)  # returns vector store of chunked doc
 st.write(db._collection.count())
+
+st.write(rag.get_procurement_answer("What are the conditions for debarment?"))
