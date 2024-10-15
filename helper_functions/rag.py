6fdd8239bcfe3524bf1254e1e0174fdf873f189c
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-15 16:42:47 +0800

    Update rag.py

--- helper_functions/rag.py
+++ helper_functions/rag.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import numpy as np
-from langchain_community.document_loaders import PyPDFLoader
+from helper_functions import llm
 
+from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-from helper_functions import llm
+from langchain_community.document_loaders import TextLoader
+from langchain_community.vectorstores import Chroma
 
 def loader(filepath):
     loader = PyPDFLoader(filepath)
@@ -19,3 +21,11 @@ def text_splitter(pages):
         length_function = llm.count_tokens
     )
     return text_chunking.split_documents(pages)
+
+def write_vector_store(splitted_documents):
+    # Load the document, split it into chunks, embed each chunk and load it into the vector store.
+    return Chroma.from_documents(
+        splitted_documents,
+        llm.embeddings_model,
+        persist_directory = "./chroma_db"
+    )
