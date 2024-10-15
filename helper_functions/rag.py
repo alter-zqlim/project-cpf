2ee76f2f92bfbd0f19880f99506be317f5d8d6e2
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-15 16:08:56 +0800

    Update rag.py

--- helper_functions/rag.py
+++ helper_functions/rag.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import numpy as np
 from langchain_community.document_loaders import PyPDFLoader
+
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 def loader(filepath):
   loader = PyPDFLoader(filepath)
