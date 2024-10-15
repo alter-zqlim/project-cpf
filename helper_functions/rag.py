fc1a7bed565df44e5d42ff5ba536115ea678b99d
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-15 22:03:06 +0800

    Update rag.py

--- helper_functions/rag.py
+++ helper_functions/rag.py
@@ -47,8 +47,8 @@ def write_vector_store(splitted_documents):
 
 def get_procurement_answer(user_query, vector_base):
     template = """You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
-    Question: {question} 
     Context: {context} 
+    Question: {question} 
     Answer:
     """
     retrieval_qa_chat_prompt = PromptTemplate.from_template(template)
@@ -56,6 +56,6 @@ def get_procurement_answer(user_query, vector_base):
     
     combine_docs_chain = create_stuff_documents_chain(large_lang_model, retrieval_qa_chat_prompt)
     rag_chain = create_retrieval_chain(vector_base, combine_docs_chain)
-    return rag_chain.invoke({"input": "What are the conditions for debarment?"})
+    return rag_chain.invoke({"input": user_query})
 
 
