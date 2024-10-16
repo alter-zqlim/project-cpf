855e9b3b20aeed9a4aaf1a2c77d0f4112f1a03b4
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-16 14:53:14 +0800

    Update rag.py

--- helper_functions/rag.py
+++ helper_functions/rag.py
@@ -47,7 +47,9 @@ def write_vector_store(splitted_documents):
 
 def get_procurement_answer(user_query, vector_base):
     template = """You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
-    Context: {context} Question: {question} Answer:
+    Context: {context}
+    Question: {question}
+    Answer:
     """
     retrieval_qa_chat_prompt = PromptTemplate.from_template(template)
     large_lang_model = ChatOpenAI(model = "gpt-3.5-turbo", openai_api_key = st.secrets["KEY_OPENAI_API"])
