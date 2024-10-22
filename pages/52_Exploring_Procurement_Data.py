9aedcb4eb906f50a32ef47a306dcb5a1d05ac6ee
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-22 21:53:24 +0800

    Update 52_Exploring_Procurement_Data.py

--- pages/52_Exploring_Procurement_Data.py
+++ pages/52_Exploring_Procurement_Data.py
@@ -39,7 +39,7 @@ df_markeddown = df.to_markdown()
 # st.bar_chart(df, y = "agency", x = "awarded_amt", y_label = "Agency", x_label = "Awarded Procurement Value")
 counts = df[data_input_index].value_counts().reset_index()
 counts.columns = [data_input_index, "count"]
-st.write(counts)
+st.bar_chart(counts, y = "agency", x = "count", y_label = "Agency", x_label = "Number of Awarded Tenders")
 
 # password checkpoint
 if not utility.check_password():  
