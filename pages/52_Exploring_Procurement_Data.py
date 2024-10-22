6b0ddea5d56a8a6069877bc9a0de582a7a0084ab
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-22 21:25:53 +0800

    Update 52_Exploring_Procurement_Data.py

--- pages/52_Exploring_Procurement_Data.py
+++ pages/52_Exploring_Procurement_Data.py
@@ -35,37 +35,5 @@ data_input_index = "agency"
 # read GeBiz data file, set index to "agency", sort by specified columns, construct index with unique values
 unsorted_df = utility.get_GeBIZ_data(data_input_filepath, data_input_index)
 df = unsorted_df.sort_values(by = ["tender_no", "supplier_name", "award_date"])
-df_index = df[~df.index.duplicated(keep = "first")]
-df_markeddown = df.to_markdown()
 
-pandas_agent = llm.init_pandas_dataframe_agent(unsorted_df)
-csv_agent = llm.init_csv_agent("./data/GovernmentProcurementviaGeBIZ.csv")
-
-# generate a multi-option selector that displays data based on selected agencies  
-agencies = st.multiselect(
-    "Select agencies",
-    list(df_index.index),
-    ["Competition and Consumer Commission of Singapore (CCCS)"]
-)
-if not agencies:
-    st.error("Please select at least one agency.")
-else:
-    data = df.loc[agencies]
-    st.write("## Procurement projects", data.sort_index())
-
-# generate a form for user input
-form = st.form(key = "form")
-form.subheader("What would you like to know about the GeBiz procurement data from FY2019 to FY2023?")
-
-user_input = form.text_area(
-    "Please enter your query below and press Submit", 
-    height = 160
-)
-
-# on detecting Submit, processes and writes response to user input
-if form.form_submit_button("Submit"):
-    st.toast(f"User Input: {user_input}")
-    response = csv_agent.invoke(user_input)
-    # response = pandas_agent.invoke(user_input)
-    # response = llm.generate_response_based_on_procurement_data(user_input, df)  # unable to use to_markdown() because of token limit
-    st.write(response["output"])
+st.dataframe(df, use_container_width = True)
