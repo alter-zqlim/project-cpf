41544004e507b6cde43674d7b3b5d9be4a3f46d3
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-25 16:53:20 +0800

    Update 52_Exploring_Procurement_Data.py

--- pages/52_Exploring_Procurement_Data.py
+++ pages/52_Exploring_Procurement_Data.py
@@ -119,4 +119,4 @@ if form.form_submit_button("Submit"):
     response = csv_agent.invoke(user_input)
     # response = pandas_agent.invoke(user_input)
     # response = llm.generate_response_based_on_procurement_data(user_input, data)  # unable to use to_markdown() because of token limit
-    st.write(response)
+    st.write(response["output"])
