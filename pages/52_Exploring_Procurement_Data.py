d66314e8dc564572dbe92c19a0357bc56dd378b1
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-26 21:07:15 +0800

    Update 52_Exploring_Procurement_Data.py

--- pages/52_Exploring_Procurement_Data.py
+++ pages/52_Exploring_Procurement_Data.py
@@ -81,6 +81,20 @@ with col_01:
     )
 
 with col_02:
+    vaulted = st.multiselect(
+        "Filter information by tender award status for selected agencies",
+        list(df.tender_detail_status.unique())[::-1]
+        # ["Award by interface record", "Awarded by Items", "Awarded to No Suppliers", "Awarded to Suppliers"]
+    )
+    # generate a radio selector that displays data based on tender status  
+    tender_status_list = list(df.tender_detail_status.unique())[::-1]
+    tender_status_list.append("All")
+    tender_status = st.radio(
+        "Filter information by tender award status for selected agencies",
+        tender_status_list,
+        len(tender_status_list) - 1
+    )
+
     # generate a radio selector that displays data based on tender status
     df["year"] = pd.to_datetime(df["award_date"], format = "%d/%m/%Y")
     list_years = df["year"].dt.year.unique().tolist()
