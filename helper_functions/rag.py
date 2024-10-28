3b93ffc1eb9f17e08210067e61d355e67a3a4c8c
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-28 08:54:47 +0800

    Update rag.py

--- helper_functions/rag.py
+++ helper_functions/rag.py
@@ -1,3 +1,8 @@
+__import__('pysqlite3')
+import sys
+import pysqlite3
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st
 import numpy as np
 from helper_functions import llm
