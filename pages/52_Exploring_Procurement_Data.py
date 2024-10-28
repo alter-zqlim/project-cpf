4a2563e57041cc284ab061032715848f994ec860
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-28 11:50:00 +0800

    Update 52_Exploring_Procurement_Data.py

--- pages/52_Exploring_Procurement_Data.py
+++ pages/52_Exploring_Procurement_Data.py
@@ -110,7 +110,7 @@ else:
 
 # generate a form for user input
 form = st.form(key = "form")
-form.subheader("What would you like to know about the above set of GeBiz procurement data?")
+form.subheader("What would you like to know about the above set of historical GeBiz procurement data?")
 
 user_input = form.text_area(
     "Please enter your query below and press Submit", 
