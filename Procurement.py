9c9350f2f961199846175919917694462d05413d
alter-zqlim <quar7z.ver4@gmail.com>
2024-10-29 22:27:12 +0800

    Update Procurement.py

--- Procurement.py
+++ Procurement.py
@@ -36,7 +36,7 @@ expander.write(EXPANDER_NOTICE)
 st.write("# For businesses: How to go about providing goods and services to the government")
 st.write("Hello Entrepreneur! Congratulations on setting up your business and we hope you see ever increasing sales! Through this web application, you can learn more about transacting with the government to deliver goods and services!")
 st.write("Note that there are **NO** procurement opportunities on this web application. Head over to https://www.gebiz.gov.sg/ to search for those opportunities.")
-st.write("Select an app on the left :material/west: to get started.")
+st.write(":material/west::material/west: Select an app on the left to get started.")
 
 st.image("./assets/image.webp")
 # manifest sidebar
